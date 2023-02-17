6459ab2e47526252c78bb09c0e61ddabd9f81d09
ann6pe <123501477+ann6pe@users.noreply.github.com>
2023-02-17 05:04:08 -0500

    added welcome to installed_apps in settings.py and created basic model for tutor

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1','a25.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'welcome.apps.WelcomeConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
