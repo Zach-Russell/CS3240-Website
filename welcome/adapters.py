a7d2584447d2c3385baac657cf72f4f774e3ce27
ann6pe <123501477+ann6pe@users.noreply.github.com>
2023-02-19 16:00:54 -0500

    custom user model, allow users to choose student/tutor on signup

--- welcome/adapters.py
+++ welcome/adapters.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.urls import reverse
+from django.shortcuts import redirect
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 class MyAccountAdapter(DefaultAccountAdapter):
+  def get_signup_redirect_url(self, request):
+     return '/googlelogin/type'
   def get_login_redirect_url(self, request):
-      return '/googlelogin/type/'
-
-class MySocialAccountAdapter(DefaultSocialAccountAdapter):
-  def get_login_redirect_url(self, request):
-      return '/googlelogin/type/'
+      if request.user.type == "":
+         return '/googlelogin/type'
+      return '/welcome'
--- welcome/models.py
+++ welcome/models.py
@@ -1,18 +1,54 @@
+from django.contrib.auth.models import AbstractBaseUser,    BaseUserManager, PermissionsMixin
 from django.db import models
-from allauth.account.signals import user_logged_in
-from django.dispatch import receiver
-class User(models.Model):
-    first_name = models.CharField(max_length=30)  
-    last_name = models.CharField(max_length=30)
-    user_type = [
-        'Tutor', 'Tutor',
-        'Student', 'Student'
-    ]
-    def __str__(self):
-        return self.first_name + " " + self.last_name
-# Create your models here.
+from django.utils import timezone
 
 
-# @receiver(user_logged_in)
-# def login_logger(request, user, **kwargs):
-#     print('jaksdlfa;sdfjaklsdfjlkasdkjlfaskjld;fkjl;adskjl;faskjl;dfkjl;adskjlfkjlasdkjlfjlasnmcvnzx')
+class UserManager(BaseUserManager):
+
+  def _create_user(self, email, password, type, is_staff, is_superuser,  **extra_fields):
+    if not email:
+        raise ValueError('Users must have an email address')
+    now = timezone.now()
+    email = self.normalize_email(email)
+    user = self.model(
+        email=email,
+        is_staff=is_staff, 
+        is_active=True,
+        is_superuser=is_superuser, 
+        last_login=now,
+        date_joined=now,
+        type = type, 
+        **extra_fields
+    )
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+
+  def create_user(self, email, password, **extra_fields):
+    return self._create_user(email, password, type, False, False, **extra_fields)
+
+  def create_superuser(self, email, password, type, **extra_fields):
+    user=self._create_user(email, password, type, True, True, **extra_fields)
+    user.save(using=self._db)
+    return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=254, unique=True)
+    first_name = models.CharField(max_length=254, null=True, blank=True)
+    last_name = models.CharField(max_length=254, null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=3, choices=[('tut', 'tutor'),('stu', 'student')], default = "")
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['type']
+
+    objects = UserManager()
+
+    def get_absolute_url(self):
+        return "/users/%i/" % (self.pk)
