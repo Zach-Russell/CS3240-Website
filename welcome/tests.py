0ad20fdbc56c3b8c613bb86ccdba297e4d8d7988
Zach-Russell <zacharyrussell01@gmail.com>
2023-03-17 17:53:51 -0400

    Try adding some new tests

--- welcome/tests.py
+++ welcome/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+#Create your tests here.
+class BasicTestingTests(TestCase):
+    def test_False_is_False(self):
+        self.assertFalse(False)
+
+    def test_True_is_True(self):
+        self.assertTrue(True)
+
+    def test_one_plus_one_is_two(self):
+        self.assertTrue(1 + 1, 2)
