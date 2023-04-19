b6261e88cde016cd4a3861df2a165ab11c83ccce
Zach-Russell <zacharyrussell01@gmail.com>
2023-04-18 22:43:31 -0400

    Add secure cookies

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,6 +80,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+
+#Secure cookies
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 DATABASES={
    'default':{
       'ENGINE':'django.db.backends.postgresql_psycopg2',
