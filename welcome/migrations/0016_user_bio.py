81764370472e703a68fc03adc89c6703e0eaf5f4
ann6pe <123501477+ann6pe@users.noreply.github.com>
2023-05-01 23:41:41 -0400

    Update 0016_user_bio.py

--- welcome/migrations/0016_user_bio.py
+++ welcome/migrations/0016_user_bio.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("welcome", "0014_classrequest_tutorsalreadyaccepted"),
+        ("welcome", "0015_alter_classrequest_tutorsalreadyaccepted"),
     ]
 
     operations = [
